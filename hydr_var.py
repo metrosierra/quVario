bacaaa9d1488c4e021594c9c616046ab0d766b30
Kenton Kwok <kwokkenton@users.noreply.github.com>
2020-05-19 23:36:08 +0800

    Managed to convert SymPy integral into something that SciPy can read

--- hydr_var.py
+++ hydr_var.py
@@ -11,12 +11,15 @@ The Hamiltonian is fixed at the moment
 #%% Import packages and initialisation of spaces
 #from scipy.misc import derivative
 from sympy.vector import CoordSys3D, Del
-from sympy import diff, exp, conjugate, symbols, integrate, solve
+from sympy import (exp, conjugate, symbols, simplify, integrate, lambdify)
+from scipy.optimize import fmin
+from scipy.integrate import quad
+import scipy as sp
 
 #initialisation of coordinate systems for vector calculus
+#we work in Spherical coordinates because of the nature of the problem
 R = CoordSys3D('R', transformation = 'spherical')
-#rlen = r.magnitude()
-delop = Del()
+delop = Del() #by convention
 
 '''
 Sympy help
@@ -26,6 +29,8 @@ Note that R.r, R.theta, R.phi are the variables called base scalars
 and R.i, R.j, R.k are the r, theta, phi basis vectors
 
 https://docs.sympy.org/latest/tutorial/basic_operations.html
+
+Symplify may be of use later
 
 '''
 #%%
@@ -41,9 +46,9 @@ inf = 10
 #Hamiltonian operator H operating on psi
 def H(psi):
     '''
-    The Hamiltonian operator operating on some trial function
-    input: some trial function psi(r)
-    output: another function
+    The Hamiltonian operator operating on some trial function psi
+    Input: some trial function psi(r)
+    Output: another scalar field Sympy expression
     '''
     H = - 1 / 2 * del2(psi) - 1 / R.r * psi
     
@@ -53,15 +58,17 @@ def H(psi):
 def del2(f):
     '''
     Laplacian operator in spherical polar coordinates
-    input: some function f and point r
-    output: 
+    Input: some Sympy expression f to which the Laplacian is applied 
+    Output: A Sympy expression
     '''
     
     del2 = delop.dot(delop(f)).doit()
     
     return del2
 
-#Trial function psi
+#Trial function psi defined using SymPy
+psi = exp(-alpha * R.r **2)
+
 #def psi():
 #    '''
 #    The trial function
@@ -70,33 +77,56 @@ def del2(f):
 #    '''
 #    return N * exp(- alpha * R.r **2)
 
-psi = exp(-alpha * R.r **2)
-
 #%% Testing area
-def f (r):
-    '''
-    Random scalar field f
-    '''
-    return 4* R.i * R.r
+#def f (r):
+#    '''
+#    Random scalar field f
+#    '''
+#    return 4* R.i * R.r
 #    try:
 #        f =  1/ (r) ** 2
 #    except ZeroDivisionError:
 #        f = 9e9999
 #        pass
 #    return f
-r = 1.0
+#r = 1.0
 #print(derivative((r ** 2) * derivative(f, 1, dx= 1e-6), r))
-    
 #print(del2(f, 1.0))
 
 
-#%%
-#The expectation value integral <psi|H|psi>
-
-expectation = conjugate(psi) * H(psi)
+#%% This part executes the variation method
+#The expectation value  <psi|H|psi>
+expectation = simplify(conjugate(psi) * H(psi) * R.r **2)
 #print(expectation)
 
 #the normalisation value <psi|psi> 
 absolute = integrate(conjugate(psi) * psi, (R.r,0,100)).evalf()
 
-solve(diff(integrate(expectation, (R.r ,0.001,inf)), alpha), alpha) 
+#The lambdify function converts Sympy expressions into something more suitable
+#for numerical evaluation with SciPy
+expectation_lamb = lambdify((R.r, alpha), expectation, 'scipy')
+
+#now define a function to be optimsied
+#def expectationfunc(r, alpha):
+#    '''
+#    This function concerts the SymPy expression with base scalars and vectors 
+#        into a Python function that can be inputted and integrated using SciPy
+#    Input:
+#    Output:
+#    '''
+#    
+#    expectationfunc = expectation.subs([(R.r, a), (r, alpha)])
+#    return expectationfunc
+#solve(diff(integrate(expectation, (R.r ,0.001,inf)), alpha), alpha) 
+
+# definining a function such that SciPy Optimisation can be used
+def expectation_intgrl(a):
+    '''
+    
+    returns 
+    '''
+    expectation_integral = quad(expectation_lamb, 0, 16 ,args=(a))
+    return expectation_integral[0]
+
+
+fmin(expectation_intgrl, 5)
