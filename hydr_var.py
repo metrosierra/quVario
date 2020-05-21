18aae6ca2c4604068f07acbcdfd759e3dd9e7ce6
Kenton Kwok <kwokkenton@users.noreply.github.com>
2020-05-21 14:54:00 +0800

    Solved! Outputs correct parameter and minimum energy.
    The previous error was not including the normalisation in the variation integral.
    Lambdified the normalisation integral function.
    yay!

--- hydr_var.py
+++ hydr_var.py
@@ -3,52 +3,53 @@
 """
 Created on Mon May 18 10:42:53 2020
 
-Variational Method for finding ground state energy of Hydrogen
-The Hamiltonian is fixed at the moment
+Variational Method for finding ground state energy of Hydrogen.
+Exercise 1.19 in Quantum Chemistry. 
+The Hamiltonian is fixed at the moment.
 
 @author: kenton
 """
 #%% Import packages and initialisation of spaces
-#from scipy.misc import derivative
+
 from sympy.vector import CoordSys3D, Del
-from sympy import (exp, conjugate, symbols, simplify, integrate, lambdify)
+from sympy import (exp, conjugate, symbols, simplify, lambdify)
 from scipy.optimize import fmin
 from scipy.integrate import quad
-import scipy as sp
+#import scipy as sp
+#import matplotlib.pyplot as plt 
 
-#initialisation of coordinate systems for vector calculus
-#we work in Spherical coordinates because of the nature of the problem
+# initialisation of coordinate systems for vector calculus
+# we work in Spherical coordinates because of the nature of the problem
+# this simplifies the need for the Laplacian as SymPy has sphericals built in
 R = CoordSys3D('R', transformation = 'spherical')
 delop = Del() #by convention
 
-'''
-Sympy help
 
-Note that R.r, R.theta, R.phi are the variables called base scalars
-    Base scalars can be treated as symbols
-and R.i, R.j, R.k are the r, theta, phi basis vectors
+#Sympy help
+#
+#Note that R.r, R.theta, R.phi are the variables called base scalars
+#    Base scalars can be treated as symbols
+#and R.i, R.j, R.k are the r, theta, phi basis vectors
+#
+#https://docs.sympy.org/latest/tutorial/basic_operations.html
 
-https://docs.sympy.org/latest/tutorial/basic_operations.html
+#%% Define symbolic parameters and numerical infinity
 
-Symplify may be of use later
-
-'''
-#%%
-#Symbolic parameters and numerical infinity
-
-#N, alpha = symbols('N alpha', real=True)
 alpha = symbols('alpha', real = True)
-#N = 1
-#alpha = 1
-inf = 10
+
+inf = 1000
 
 #%% Define functions
+
 #Hamiltonian operator H operating on psi
 def H(psi):
-    '''
-    The Hamiltonian operator operating on some trial function psi
-    Input: some trial function psi(r)
-    Output: another scalar field Sympy expression
+    ''' Function: the Hamiltonian operator operating on some trial function psi
+   
+    Input: 
+        some trial function psi(r)
+    
+    Output: 
+        another scalar field Sympy expression
     '''
     H = - 1 / 2 * del2(psi) - 1 / R.r * psi
     
@@ -56,77 +57,119 @@ def H(psi):
 
 #Del-squared, the Laplacian for the Schrodinger equation
 def del2(f):
-    '''
-    Laplacian operator in spherical polar coordinates
-    Input: some Sympy expression f to which the Laplacian is applied 
-    Output: A Sympy expression
+    '''Laplacian operator in spherical polar coordinates
+    
+    Input: 
+        some Sympy expression f to which the Laplacian is applied 
+    
+    Output: 
+        A Sympy expression
     '''
     
     del2 = delop.dot(delop(f)).doit()
     
     return del2
 
-#Trial function psi defined using SymPy
+# Trial function psi defined using SymPy
+# A decaying Gaussian 
 psi = exp(-alpha * R.r **2)
 
-#def psi():
-#    '''
-#    The trial function
-#    input:
-#    output: 
-#    '''
-#    return N * exp(- alpha * R.r **2)
+# We need to supply the Jacobian manually because we are no longer integrating 
+# with SymPy.
+# The Jacobian of the spherical integral is r**2 sin(theta)
+# ignore sin(theta) because of symmetry 
+jacobian = R.r ** 2 
 
-#%% Testing area
-#def f (r):
-#    '''
-#    Random scalar field f
-#    '''
-#    return 4* R.i * R.r
-#    try:
-#        f =  1/ (r) ** 2
-#    except ZeroDivisionError:
-#        f = 9e9999
-#        pass
-#    return f
-#r = 1.0
-#print(derivative((r ** 2) * derivative(f, 1, dx= 1e-6), r))
-#print(del2(f, 1.0))
-
-
-#%% This part executes the variation method
-#The expectation value  <psi|H|psi>
-expectation = simplify(conjugate(psi) * H(psi) * R.r **2)
-#print(expectation)
-
-#the normalisation value <psi|psi> 
-absolute = integrate(conjugate(psi) * psi, (R.r,0,100)).evalf()
+#%% This part defines function related to the variational method
 
 #The lambdify function converts Sympy expressions into something more suitable
 #for numerical evaluation with SciPy
-expectation_lamb = lambdify((R.r, alpha), expectation, 'scipy')
 
-#now define a function to be optimsied
-#def expectationfunc(r, alpha):
-#    '''
-#    This function concerts the SymPy expression with base scalars and vectors 
-#        into a Python function that can be inputted and integrated using SciPy
-#    Input:
-#    Output:
-#    '''
-#    
-#    expectationfunc = expectation.subs([(R.r, a), (r, alpha)])
-#    return expectationfunc
-#solve(diff(integrate(expectation, (R.r ,0.001,inf)), alpha), alpha) 
+#The expectation value  <psi|H|psi>
+expectation = simplify(conjugate(psi) * H(psi) ) 
+
+expectation_lamb = lambdify((R.r, alpha), expectation * jacobian, 'scipy')
+
+#the normalisation value <psi|psi> 
+norm = conjugate(psi) * psi 
+
+norm_lamb = lambdify((R.r, alpha), norm * jacobian, 'scipy')
+
 
 # definining a function such that SciPy Optimisation can be used
-def expectation_intgrl(a):
+def expectation_intgrl(a, stpt, endpt):
+    ''' Formualtes an integral of the lambdified expectation function
+   
+    inputs:
+        a (float): parameter
+            
+        stpt (float): starting point of the integral 
+        
+        endpt (float): endpoint of the integral
+    
+    output: 
+        expectation_integral[0]: numerical result of the integral
+    
     '''
     
-    returns 
-    '''
-    expectation_integral = quad(expectation_lamb, 0, 16 ,args=(a))
+    expectation_integral = quad(expectation_lamb, 0, endpt,args=(a))
+    
     return expectation_integral[0]
 
+def norm_intgrl(a, stpt, endpt):
+    ''' Finds the square of the Normalisation constant of the trial function.
+    
+    Formualtes an integral of the lambdified normalisation function and uses
+    the fact that the trial functions must be normalised to 1. 
+   
+    inputs:
+        a (float): 
+            
+        stpt (float): starting point of the integral
+        
+        endpt (float): endpoint of the integral
+    
+    output: 
+        N_squared: normalisation
+    
+    '''
+    norm_intgrl = quad(norm_lamb, stpt, endpt, args = (a)) 
+    
+    N_squared = 1 / norm_intgrl[0]
+    
+    return N_squared
 
-fmin(expectation_intgrl, 5)
+
+def var_intgrl(a, expectation_intgrl, norm_intgrl, stpt = 0, endpt = 1000):
+    ''' Multiplies the normalisation with the expectation integral
+    
+    inputs: 
+        expectation_intgrl (function of a): 
+            
+        norm_intgrl (function of a): N_squared, normalisation for trial function
+        
+        stpt (float): starting point of the integral (0 by default)
+        
+        endpt (float): endpoint of the integral (1000 effectively infinity)
+        
+    outputs: 
+        var_intgrl (function of a): numerical result of the 
+            variation integral
+    '''
+    
+    var_intgrl = (norm_intgrl(a, stpt, endpt) * 
+                  expectation_intgrl(a, stpt, endpt))
+    
+    return var_intgrl
+
+
+#%% Execution of the minimisation of the variation integral
+
+#supply the initial guess for the variation intergral
+initial_guess = 0.1
+
+#minimise the variation integral with respect to the parameter
+alpha_op, energy_min = fmin(var_intgrl, initial_guess, (expectation_intgrl, norm_intgrl), 
+              full_output = 1)[0:2]
+
+print('\nThe optimised parameter is %f. \nThe minimum energy is %f.' % (float(alpha_op),energy_min))
