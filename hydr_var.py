e4ce41170a2570df9c4d29e3a04a36af456e2a19
Kenton Kwok <kwokkenton@users.noreply.github.com>
2020-05-22 12:18:15 +0800

    Added ability to try more trial functions
    Added functionality for trying different trial functions and parsing the input in a text doc

--- hydr_var.py
+++ hydr_var.py
@@ -10,9 +10,9 @@ The Hamiltonian is fixed at the moment.
 @author: kenton
 """
 #%% Import packages and initialisation of spaces
-
-from sympy.vector import CoordSys3D, Del
 from sympy import (exp, conjugate, symbols, simplify, lambdify)
+from sympy.vector import CoordSys3D, Del
+from sympy.parsing.sympy_parser import parse_expr
 from scipy.optimize import fmin
 from scipy.integrate import quad
 #import scipy as sp
@@ -24,7 +24,6 @@ from scipy.integrate import quad
 R = CoordSys3D('R', transformation = 'spherical')
 delop = Del() #by convention
 
-
 #Sympy help
 #
 #Note that R.r, R.theta, R.phi are the variables called base scalars
@@ -35,7 +34,7 @@ delop = Del() #by convention
 
 #%% Define symbolic parameters and numerical infinity
 
-alpha = symbols('alpha', real = True)
+alpha = symbols('alpha', real = 0)
 
 inf = 1000
 
@@ -71,34 +70,47 @@ def del2(f):
     
     return del2
 
-# Trial function psi defined using SymPy
-# A decaying Gaussian 
-psi = exp(-alpha * R.r **2)
-
 # We need to supply the Jacobian manually because we are no longer integrating 
 # with SymPy.
 # The Jacobian of the spherical integral is r**2 sin(theta)
 # ignore sin(theta) because of symmetry 
 jacobian = R.r ** 2 
 
+def readtrialfuncs(txtdoc = 'hydr_var_trialfuncs.txt'):
+    ''' Reads a text document of trial functions and creates a sympy list
+    
+    inputs:
+        txtdoc: text document filename
+        
+    outputs:
+        trialfuns: a list of trial functions
+        
+    '''
+    
+    f = open(txtdoc, "r")
+    
+    trialfuncs = []
+    
+    for line in f:
+        x = parse_expr(line)
+        
+        x = x.subs(symbols('r'), R.r)
+        
+        trialfuncs.append(x)
+        
+    f.close()
+    
+    return trialfuncs
+
+# Trial function psi defined using SymPy
+# A decaying Gaussian 
+#psi = exp(-alpha * R.r**2)
+
+
 #%% This part defines function related to the variational method
 
-#The lambdify function converts Sympy expressions into something more suitable
-#for numerical evaluation with SciPy
-
-#The expectation value  <psi|H|psi>
-expectation = simplify(conjugate(psi) * H(psi) ) 
-
-expectation_lamb = lambdify((R.r, alpha), expectation * jacobian, 'scipy')
-
-#the normalisation value <psi|psi> 
-norm = conjugate(psi) * psi 
-
-norm_lamb = lambdify((R.r, alpha), norm * jacobian, 'scipy')
-
-
 # definining a function such that SciPy Optimisation can be used
-def expectation_intgrl(a, stpt, endpt):
+def expectation_intgrl(expectation_lamb, a, stpt, endpt):
     ''' Formualtes an integral of the lambdified expectation function
    
     inputs:
@@ -117,7 +129,8 @@ def expectation_intgrl(a, stpt, endpt):
     
     return expectation_integral[0]
 
-def norm_intgrl(a, stpt, endpt):
+
+def norm_intgrl(norm_lamb, a, stpt, endpt):
     ''' Finds the square of the Normalisation constant of the trial function.
     
     Formualtes an integral of the lambdified normalisation function and uses
@@ -141,7 +154,7 @@ def norm_intgrl(a, stpt, endpt):
     return N_squared
 
 
-def var_intgrl(a, expectation_intgrl, norm_intgrl, stpt = 0, endpt = 1000):
+def var_intgrl(a, expectation_lamb, norm_lamb, stpt = 0, endpt = 1000):
     ''' Multiplies the normalisation with the expectation integral
     
     inputs: 
@@ -158,19 +171,66 @@ def var_intgrl(a, expectation_intgrl, norm_intgrl, stpt = 0, endpt = 1000):
             variation integral
     '''
     
-    var_intgrl = (norm_intgrl(a, stpt, endpt) * 
-                  expectation_intgrl(a, stpt, endpt))
+    var_intgrl = (norm_intgrl(norm_lamb, a, stpt, endpt) * 
+                  expectation_intgrl(expectation_lamb, a, stpt, endpt))
     
     return var_intgrl
 
 
+def energy(psi):
+    ''' Obtains the optimised parameter and upper bound of the energy of the system
+    
+    inputs: 
+        trial function psi (sympy expression) that satisfies the BCs 
+    
+    outputs: 
+        alpha_op and energy_max (tuple)
+    
+    '''
+    #The expectation value  <psi|H|psi>
+    expectation = simplify(conjugate(psi) * H(psi) ) 
+    
+    expectation_lamb = lambdify((R.r, alpha), expectation * jacobian, 'scipy')
+    
+    #the normalisation value <psi|psi> 
+    norm = conjugate(psi) * psi 
+    
+    norm_lamb = lambdify((R.r, alpha), norm * jacobian, 'scipy')
+    
+    #supply the initial guess for the variation intergral
+    initial_guess = 0.1
+    
+    #minimise the variation integral with respect to the parameter
+    alpha_op, energy_max = fmin(var_intgrl, 
+                            initial_guess, (expectation_lamb, norm_lamb), 
+                            disp= 0, full_output = 1)[0:2]
+    
+    return (alpha_op, energy_max)
+
+def main():
+    ''' Main function, obtains the lowest energy of all trial functions and prints
+        the intermediate energies
+    
+    outputs: overall_energy_min
+        
+    '''
+    energies = []
+    
+    print('In atomic units,')
+    
+    for psi in readtrialfuncs():
+        
+        alpha_op, energy_min = energy(psi)
+        
+        energies.append(energy_min)
+        
+        print('\nFor psi = %s, \nThe optimised parameter is %f. \nThe energy upper bound is %f.' % (psi, float(alpha_op),energy_min))
+    
+    overall_energy_min = min(energies)
+    
+    return overall_energy_min
+
+
 #%% Execution of the minimisation of the variation integral
 
-#supply the initial guess for the variation intergral
-initial_guess = 0.1
-
-#minimise the variation integral with respect to the parameter
-alpha_op, energy_min = fmin(var_intgrl, initial_guess, (expectation_intgrl, norm_intgrl), 
-              full_output = 1)[0:2]
-
-print('\nIn atomic units, \nThe optimised parameter is %f. \nThe minimum energy is %f.' % (float(alpha_op),energy_min))
+print('\nThe overall maximum energy is %f.' %main())
