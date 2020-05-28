7f030cc6e63803dba961a4a322ee7c8e2e3da648
Wu Mingsong <mingsong123@gmail.com>
2020-05-29 01:10:24 +0800

    Renamed files to group optimisation methods and function generators

--- helium_markii/optipack.py
+++ helium_markii/optipack.py
@@ -5,8 +5,12 @@
 # github.com/starryblack/quVario
 
 
-### This script provides the montyCarlo object that handles any calls to monte carlo integration functions of our design. It interfaces with the python packages installed for basic functionalities (ie mcint)
-### montycarlo should serve helium_markii.py which is the higher level script for direct user interface
+### This script provides the montyCarlo object AND miniMiss object that handles any calls to monte carlo integration functions and minimisation functions of our design. It interfaces with the python packages installed for basic functionalities (ie mcint)
+### optipack should serve helium_markii.py which is the higher level script for direct user interface
+
+
+### as of 27 may 2020 montycarlo uses mcint, and minimiss uses scipy fmin functions as main work horses.
+
 
 import math
 import sys
@@ -49,123 +53,76 @@ class montyCarlo():
 
 #        self.macro1()
         print(self.integrator_basic())
-        print(self.integrator_mc())
 #        print(self.sampler(self.bounds))
 
     def get_measure(self, bounds):
         ''' obtains n dimensional 'measure' for the integral, which effectively
-            is the volume in n dimensional space of the integral bounds. used for 
-            multiplying the expected value. 
-            
-            inputs: 
+            is the volume in n dimensional space of the integral bounds. used for
+            multiplying the expected value.
+
+            inputs:
                 bounds: list of size n, indicating the n bounds of the definite
-                    integral 
-            
-            outputs: 
+                    integral
+
+            outputs:
                 measure: float
-                
+
         '''
         measure = 1
-        
+
         for i in bounds:
             endpt = i[1]
             stpt = i[0]
-            dimlength = endpt - stpt 
-            
+            dimlength = endpt - stpt
+
             measure *= dimlength
-                
+
         return measure
 
     def integrator_basic(self):
-        ''' using mcint package to determine the integral, crudely
-        
+        '''
+        using mcint package to determine the integral, crudely
+
         '''
         # measure is the 'volume' over the region you are integrating over
-        
+
         result,error = mcint.integrate(self.integrand,self.sampler(self.bounds),
                                         self.get_measure(self.bounds), self.n)
-        
+
         return result
-    
-    #UNFINISHED
-#    def integrator_mc(self):
-#        ''' using mcint package to determine the integral, crudely
-#        
-#        '''
-#        # measure is the 'volume' over the region you are integrating over
-#        
-#        result,error = mcint.integrate(self.mcintegrand, self.metropolis_hastings(),
-#                                        self.get_measure(self.bounds), self.n)
-        
+
     def sampler(self, bounds):
         ''' generates a tuple of n input values from a random uniform distribution
             e.g. for three dimensions, outputs tuple = (x,y,z) where x,y,z are
             floats from a uniorm distribution
-        
-            inputs: 
+
+            inputs:
                 bounds
-            
-            outputs: 
+
+            outputs:
                 sample (tuple)
         '''
         while True:
-            
+
             sample = ()
-            
-            for i in bounds: 
+
+            for i in bounds:
                 endpt = i[1]
-                stpt = i[0]     
+                stpt = i[0]
                 dimsample = random.uniform(stpt,endpt)
-                
+
                 x = list(sample)
                 x.append(dimsample)
                 sample = tuple(x)
-                
+
             yield sample
-    
 
-    def metropolis_hastings(p, iter=1000, initial_pt = [0.,0.,0.,0.,0.,0.], dims = 6):
-        ''' Metropolis algorithm for sampling from a function p
-        
-            inputs: 
-                
-            outputs: 
-    
-        '''
-        if len(initial_pt) != dims:
-            raise Exception('Error with inputs')
-            
-        # note: initial point chosen in input
-        samples = np.zeros((iter, dims))
-        
-        # now sample iter number of points
-        for i in range(iter):
-            
-            # we propose a new point using a Symmetric transition distribution 
-            #function: a Gaussian
-            proposed_pt = np.array(initial_pt) + np.random.normal(size=dims)
-            
-            # if the ratio is greater than one, accepept the proposal
-            # else, accept with probability of the ratio
-            if np.random.rand() < p(proposed_pt) / p(initial_pt):
-                initial_pt = proposed_pt
-                
-            samples[i] = np.array(initial_pt)
-    
-        return samples
-
-        
     def integrand(self, x):
         ''' this is the integrand function
-        
+
         '''
 
         return sp.exp(-(x[0]*x[1]*x[2]) ** 2)  #x**2
-    
-    def mcintegrand(self,x):
-        '''
-        '''
-        return sp.exp(-(x[0]*x[1]) ** 2)  #x**2
 
     def __enter__(self):
         pass
--- helium_markii/psiham.py
+++ helium_markii/psiham.py
@@ -8,3 +8,7 @@
 ### This script provides the hamLet object which handles calls for functions pertaining to hamiltonian generation (supposed to be general, but this case our focus is helium 5 term hamiltonian)
 
 ### hamiltonian should serve helium_markii.py which is the higher level script for direct user interface
+
+### This script also provides the psiLet object which handles calls for functions related to creating a trial wavefunction object. It MUST be compatible with hamiltonian such that the objects can interact to give expectation term (quantum chem)
+
+### psifunction should serve helium_markii.py which is the higher level script for direct user interface
